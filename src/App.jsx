faf03cebf9b0828aff921d2459ce0a4004798a63
vasanth-tech-byclarity <vasanthkr@byclaritytech.com>
2025-04-01 19:47:33 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,22 @@
 
 // export default App
 
-// ✅
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "./components/Login";
 import Appointment from "./components/Appointment";
 import LandingPage from "./components/LandingPage";
-import "./App.css";
 
 function App() {
   return (
-    <Router basename="/BerryDial">
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/appointment" element={<Appointment />} />
-        </Routes>
-      </Router>
+      </Routes>
+    </Router>
   );
 }
 
