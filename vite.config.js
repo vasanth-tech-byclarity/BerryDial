5244a9e807d7e5a3986956a1b177737ea96a8126
vasanth-tech-byclarity <vasanthkr@byclaritytech.com>
2025-04-08 22:46:04 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/conversations': {
+        target: 'http://15.207.173.143',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/conversations/, '/conversations'  ),  
+      },
+      '/audio': {
+        target: 'http://15.207.173.143',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/audio/, '/audio'),
+      },
+      
+    }
+  }
 })
